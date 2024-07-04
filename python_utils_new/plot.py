c3542d68d2e7dd07eba5cc154f4aaef241757be1
RoboTeam PC <roboteampc@gmail.com>
2024-07-04 20:44:36 +0200

    plotting for acc test

--- python_utils_new/plot.py
+++ python_utils_new/plot.py
@@ -61,22 +61,53 @@ def main() -> None:
     
     # Ensure the first timestamp is consistent across all plots
     first_timestamp = robot_commands[0].timestamp
-    try:
-        if args.input_file != 'latest.rembin':
-            observer_data = pd.read_csv(args.input_file.replace('.bin', '.csv').replace('.log', '.observer'))
-        else:
-            observer_data = pd.read_csv('latest_observer.csv')
-        observer_data = observer_data[observer_data['timestamp'] >= first_timestamp]
-        observer_data['timestamp'] = (observer_data['timestamp'] - first_timestamp) / 1000
-        observer_data['rho'] = (observer_data['velocity_x']**2 + observer_data['velocity_y']**2)**0.5
-        observer_data['theta'] = np.arctan2(observer_data['velocity_y'], observer_data['velocity_x'])
-    except:
-        observer_data = None
+    # try:
+    if args.input_file != 'latest.rembin':
+        observer_data = pd.read_csv(args.input_file.replace('.bin', '.csv').replace('.log', '.observer'))
+    else:
+        observer_data = pd.read_csv('latest_observer.csv')
+    observer_data = observer_data[observer_data['timestamp'] >= first_timestamp]
+    observer_data['timestamp'] = (observer_data['timestamp'] - first_timestamp) / 1000
+    observer_data['rho'] = (observer_data['velocity_x']**2 + observer_data['velocity_y']**2)**0.5
+    observer_data['theta'] = np.arctan2(observer_data['velocity_y'], observer_data['velocity_x'])
 
     # Extract and plot 'rho' values
     t_rc, rho_rc = extract_values(robot_commands, 'rho', first_timestamp)
     t_rf, rho_rf = extract_values(robot_feedback, 'rho', first_timestamp)
     plot_values(t_rc, rho_rc, t_rf, rho_rf, 'rho', observer_data)
+
+    # t_rc, rho_rc = extract_values(robot_commands, 'acceleration_angle', first_timestamp)
+    # t_rf, rho_rf = extract_values(robot_feedback, 'rho', first_timestamp)
+    # plot_values(t_rc, rho_rc, t_rf, rho_rf, 'rho', observer_data)
+
+    figure = plt.figure(figsize=(10, 6))
+    figure.canvas.toolbar.zoom()
+    plt.plot(t_rc, rho_rc, label="Reference rho", linewidth=2, linestyle='--')
+    plt.plot(t_rf, rho_rf, label="Achieved rho", linewidth=2, linestyle='-')
+    if observer_data is not None and 'rho' in observer_data.columns:
+        plt.plot(observer_data['timestamp'], observer_data['rho'], label="Observer rho", linewidth=2, linestyle=':')
+
+    t_rc, acc_ang_rc = extract_values(robot_commands, 'acceleration_angle', first_timestamp)
+    # plt.plot(t_rc, acc_ang_rc, label="Acc ang", linewidth=2, linestyle='--')
+    t_rc, acc_mag_rc = extract_values(robot_commands, 'acceleration_magnitude', first_timestamp)
+
+    acc_x = []
+    acc_y = []
+    for i in range(len(acc_ang_rc)):
+        acc_x.append(np.cos(acc_ang_rc[i]) * acc_mag_rc[i])
+        acc_y.append(np.sin(acc_ang_rc[i]) * acc_mag_rc[i])
+
+    # print(acc_y)
+    # print(acc_mag_rc)
+    plt.plot(t_rc, acc_x, label="Acc x", linewidth=2, linestyle='--')
+    plt.plot(t_rc, acc_y, label="Acc y", linewidth=2, linestyle='--')
+
+    
+    plt.xlabel("Timestamp (s)", fontsize=14)
+    plt.ylabel('haha yes', fontsize=14)
+    plt.legend(fontsize=12)
+    plt.grid(True)
+    plt.tight_layout()
     
     # Extract and plot 'theta' values
     t_rc, theta_rc = extract_values(robot_commands, 'theta', first_timestamp)
@@ -87,6 +118,18 @@ def main() -> None:
     tRef_si, wheel_speed_ref_1_si = extract_values(robot_state_info, 'wheelSpeedRef1', first_timestamp)
     t_si, wheel_speed_1_si = extract_values(robot_state_info, 'wheelSpeed1', first_timestamp)
     plot_values(tRef_si, wheel_speed_ref_1_si, t_si, wheel_speed_1_si, 'wheelSpeed1')
+
+    tRef_si, wheel_speed_ref_1_si = extract_values(robot_state_info, 'wheelSpeedRef2', first_timestamp)
+    t_si, wheel_speed_1_si = extract_values(robot_state_info, 'wheelSpeed2', first_timestamp)
+    plot_values(tRef_si, wheel_speed_ref_1_si, t_si, wheel_speed_1_si, 'wheelSpeed2')
+
+    tRef_si, wheel_speed_ref_1_si = extract_values(robot_state_info, 'wheelSpeedRef3', first_timestamp)
+    t_si, wheel_speed_1_si = extract_values(robot_state_info, 'wheelSpeed3', first_timestamp)
+    plot_values(tRef_si, wheel_speed_ref_1_si, t_si, wheel_speed_1_si, 'wheelSpeed3')
+
+    tRef_si, wheel_speed_ref_1_si = extract_values(robot_state_info, 'wheelSpeedRef4', first_timestamp)
+    t_si, wheel_speed_1_si = extract_values(robot_state_info, 'wheelSpeed4', first_timestamp)
+    plot_values(tRef_si, wheel_speed_ref_1_si, t_si, wheel_speed_1_si, 'wheelSpeed4')
     plt.show()
 
 if __name__ == "__main__":
