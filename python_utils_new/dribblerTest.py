59ef126f0dc2cdde9e5b74a569e38b5fb0c1a2ba
Jonaplant <jonathan.jeuring@proton.me>
2025-04-29 12:36:31 +0200

    add dribbler test

---
+++ python_utils_new/dribblerTest.py
@@ -0,0 +1,206 @@
+import argparse
+import atexit
+import datetime
+import math
+import os
+import sys
+import time
+import numpy as np
+
+# Add parent directory to path to allow importing from Core.Inc
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Import roboteam embedded messages
+from Core.Inc.roboteam_embedded_messages.python import REM_BaseTypes as BaseTypes
+from Core.Inc.roboteam_embedded_messages.python.REM_RobotFeedback import REM_RobotFeedback
+from Core.Inc.roboteam_embedded_messages.python.REM_RobotCommand import REM_RobotCommand
+from Core.Inc.roboteam_embedded_messages.python.REM_Log import REM_Log
+from Core.Inc.roboteam_embedded_messages.python.REM_RobotStateInfo import REM_RobotStateInfo
+from visualization import visualize
+# Import local modules
+from REMParser import REMParser
+import utils
+
+basestation = None
+BASESTATION_FREQUENCY = 60
+
+
+
+def rotate(origin, point, angle):
+	ox, oy = origin
+	px, py = point
+
+	qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+	qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+	return qx, qy
+
+def close_basestation() -> None:
+	"""
+	Closes the basestation on exit.
+	"""
+	global basestation
+	if basestation is not None:
+		basestation.close()
+		print("Basestation closed, enjoy your day")
+
+atexit.register(close_basestation)
+
+def create_robot_command(test: str, robot_id: int, tick_number: int) -> REM_RobotCommand:
+	"""
+	Creates a robot command for a given robot ID.
+
+	Args:
+		test (str): The test to run.
+		tick_number (int): The current tick number.
+
+	Returns:
+		REM_RobotCommand: The created robot command.
+		
+	"""
+
+	cmd = utils.generate_empty_robot_command()
+	cmd.toRobotId = robot_id
+
+	#test order: forward > backward > left > right > spin-left > spin-right
+
+	int start_tick_number = tick_number
+	int elapsed_ticks = tick_number - start_tick_number
+
+	cmd.dribblerOn = 0
+
+	if elapsed_ticks <= 120: #forward
+		cmd.theta = 0
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+	elif elapsed_ticks > 120 and <= 240: #backward
+		cmd.theta = -math.pi
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+	elif elapsed_ticks > 240 and <= 360: #left
+		cmd.theta = -math.pi/2
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+	elif elapsed_ticks > 360 and <= 480: #right
+		cmd.theta = math.pi/2
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+	elif elapsed_ticks > 480 and <= 600:
+		cmd.yaw = -math.pi + math.pi/2 * (int(tick_number / 30) % 4)
+	elif elapsed_ticks > 600 and <= 720:
+		cmd.yaw = -math.pi + math.pi/2 * (int(tick_number / 30) % 4)
+
+	if test == "nothing":
+		cmd.rho = 0
+		cmd.theta = 0
+		cmd.angularVelocity = 0
+		cmd.useYaw = False
+	elif test == "kicker":
+		if tick_number % 120 < 10:
+			cmd.doKick = 1
+			cmd.doForce = 1 # Ignore ball sensor
+			cmd.kickChipPower = 6
+	elif test == "chipper":
+		if tick_number % 120 < 10:
+			cmd.doChip = 1
+			cmd.doForce = 1
+			cmd.kickChipPower = 6
+	elif test == "dribbler":
+		cmd.dribblerOn = 1
+	elif test == "rotate":
+		# Full rotation every 2 seconds
+		cmd.yaw = -math.pi + 2 * math.pi * ((tick_number / 120 + 0.5) % 1)
+	elif test == "forward":
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+		cmd.theta = -math.pi if tick_number % 120 < 60 else 0
+	elif test == "sideways":
+		cmd.theta = math.pi/2
+		cmd.rho = 0.3 - 0.3 * math.cos( 4 * math.pi * tick_number / 120 )
+		cmd.theta = -math.pi/2 if tick_number % 120 < 60 else math.pi/2
+	elif test == "rotate-discrete":
+		cmd.yaw = -math.pi + math.pi/2 * (int(tick_number / 30) % 4)
+	elif test == "angular-velocity":
+		cmd.angularVelocity = math.pi
+		cmd.useYaw = False
+	elif test == "circle":
+		cmd.rho = 1
+		cmd.theta = 2 * math.pi * tick_number / 240
+	elif test == "circle-forward":
+		# move in a circle while facing forward
+		cmd.rho = 1
+		cmd.theta = 2 * math.pi * tick_number / 240
+		cmd.yaw = 2 * math.pi * tick_number / 240
+
+
+	return cmd
+
+def parse_and_process_args() -> argparse.Namespace:
+	"""
+	Parse command line arguments and process related logic.
+	"""
+	global basestation
+	testsAvailable = ["nothing", "kicker", "chipper", "dribbler", "rotate", "forward", "sideways", "rotate-discrete", "angular-velocity", "circle", "circle-forward"]
+	parser = argparse.ArgumentParser()
+	parser.add_argument("robot_ids", type=int, nargs='+', help="An array of integers for the robot ids")
+	parser.add_argument("test", choices=testsAvailable, default="nothing", help="Specify which test to run. Default is 'nothing'.")
+	parser.add_argument('--output-dir', '-d', help="REMParser output directory. Logs will be placed under 'logs/OUTPUT_DIR'")
+	parser.add_argument('--simulate', action='store_true', help="Use a fake basestation for simulation")
+	args = parser.parse_args()
+
+	if args.simulate:
+		basestation = utils.open_simulated_basestation()
+		print("Simulated basestation opened")
+	elif (basestation is None or not basestation.isOpen()):
+		basestation = utils.open_continuous(timeout=0.1)
+		print("Basestation opened")
+
+	return args
+
+def main() -> None:
+	"""
+    Main function
+    """
+	global basestation
+	args = parse_and_process_args()
+	datetime_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+	output_file = None
+	if args.output_dir is not None:
+		os.makedirs(f"logs/{args.output_dir}", exist_ok=True)
+		output_file = f"logs/{args.output_dir}/log_{datetime_str}.bin"
+	parser = REMParser(basestation, output_file=output_file)
+	last_tick_time = time.time()
+	latest_feedback_time = time.time()
+	tick_number = 0
+	last_packet_feedback = None
+		last_packet_state_info = None
+	cmd = None
+	image_vis = np.zeros((500, 500, 3), dtype=float)
+	while True:
+		current_time = time.time()
+		time_till_next_tick = last_tick_time + 1/BASESTATION_FREQUENCY - current_time
+		if time_till_next_tick > 0.1 / BASESTATION_FREQUENCY:
+			time.sleep(0.1 / BASESTATION_FREQUENCY)
+		if time_till_next_tick < 0:
+			if (current_time - latest_feedback_time > 1) and (tick_number % BASESTATION_FREQUENCY == 0):
+				print("\033[93mNo feedback received in the last second\033[0m")
+			last_tick_time += 1/BASESTATION_FREQUENCY
+			tick_number += 1
+			cmd = create_robot_command(args.test, robot_id, tick_number)
+			for robot_id in args.robot_ids:
+				cmd.toRobotId = robot_id
+				basestation.write(cmd)
+				parser.write_bytes(cmd.encode())
+		parser.read()
+		parser.process()
+		while parser.has_packets():
+			packet = parser.get_next_packet()
+			if isinstance(packet, REM_RobotFeedback):
+				# print(f"Received feedback from robot {packet.fromRobotId}")
+				last_packet_feedback = packet
+				latest_feedback_time = time.time()
+			elif isinstance(packet, REM_RobotStateInfo):
+				# print(f"Received state info from robot {packet.fromRobotId}")
+				last_packet_state_info = packet
+			elif isinstance(packet, REM_Log):
+				print(packet.message)
+
+
+		# ========== VISUALISING ========== #
+		image_vis = visualize(args, image_vis, last_packet_feedback, last_packet_state_info, cmd)
+     
+if __name__ == "__main__":
+	main()
