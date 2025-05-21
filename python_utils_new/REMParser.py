7a2c7e96942ca2183262be4135c0989423998066
Hamza <h.elkady@roboteamtwente.nl>
2025-05-21 14:15:09 +0200

    use the newjoystick.py

---
+++ python_utils_new/REMParser.py
@@ -0,0 +1,222 @@
+import argparse
+import json
+import utils
+import os
+import sys
+from collections import deque
+from datetime import datetime, timedelta
+from typing import Any, BinaryIO, Deque, Dict, Optional, Type
+import numpy as np
+from serial import Serial
+# Add parent directory to path to allow importing from Core.Inc
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Import roboteam embedded messages
+from Core.Inc.roboteam_embedded_messages.python import REM_BaseTypes as BaseTypes
+from Core.Inc.roboteam_embedded_messages.python.REM_RobotFeedback import REM_RobotFeedback
+from Core.Inc.roboteam_embedded_messages.python.REM_Packet import REM_Packet
+
+class REMParser:
+	def __init__(self, device: Serial, output_file: Optional[str] = None) -> None:
+		"""
+		Initializes a new REMParser.
+
+		Args:
+			device (Serial): The device to parse REM from.
+			output_file (Optional[str], optional): The file to output parsed REM to. Defaults to None.
+		"""
+		print("[REMParser] New REMParser")
+		if device is not None:
+			print(f"[REMParser] Device {device.port}")
+
+		self.device: Serial = device
+		self.byte_buffer: bytes = bytes()
+		self.packet_buffer: Deque = deque()
+		self.output_file: Optional[BinaryIO] = None
+
+		if output_file:
+			current_dir = os.path.dirname(os.path.abspath(__file__))
+			output_file_path = os.path.join(current_dir, output_file)
+			print(f"\033[92m[REMParser] Creating output file {output_file_path}\033[0m")
+			self.output_file = open(output_file_path, "wb")
+			latest_file_path = os.path.join(current_dir, "latest.rembin")
+			if os.path.lexists(latest_file_path):
+				os.remove(latest_file_path)
+			os.symlink(output_file_path, latest_file_path)
+
+	def read(self) -> None:
+		"""
+		Reads bytes from the device if any are available and appends them to the byte buffer.
+		"""
+		bytes_in_waiting: int = self.device.inWaiting()
+		if bytes_in_waiting > 0:
+			self.byte_buffer += self.device.read(bytes_in_waiting)
+
+	def process(self) -> None:
+		"""
+		Processes the byte buffer, decoding packets and adding them to the packet buffer.
+		"""
+		while self.byte_buffer:
+			packet_type = self.byte_buffer[0]
+			packet_valid = BaseTypes.REM_PACKET_TYPE_TO_VALID(packet_type)
+			if not packet_valid:
+				self.byte_buffer = bytes()
+				continue
+			if len(self.byte_buffer) < BaseTypes.REM_PACKET_SIZE_REM_PACKET:
+				break
+
+			packet = REM_Packet()
+			packet.decode(self.byte_buffer[:BaseTypes.REM_PACKET_SIZE_REM_PACKET])
+
+			rem_packet_size = BaseTypes.REM_PACKET_TYPE_TO_SIZE(packet.packetType)
+
+			if len(self.byte_buffer) < packet.payloadSize: 
+				break
+
+			if packet.packetType != BaseTypes.REM_PACKET_TYPE_REM_LOG and packet.payloadSize != rem_packet_size:
+				self.byte_buffer = bytes()
+				continue
+
+			packet_bytes = self.byte_buffer[:packet.payloadSize]
+			packet = BaseTypes.REM_PACKET_TYPE_TO_OBJ(packet_type)()
+			packet.decode(packet_bytes)
+
+			if packet.packetType == BaseTypes.REM_PACKET_TYPE_REM_LOG:
+				message = packet_bytes[BaseTypes.REM_PACKET_SIZE_REM_LOG:]
+				packet.message = message.decode()
+
+			self.add_packet(packet)
+			self.write_bytes(packet_bytes)
+			self.byte_buffer = self.byte_buffer[packet.payloadSize:]
+
+	def add_packet(self, packet: Any) -> None:
+		"""
+		Adds a packet to the packet buffer.
+
+		Args:
+			packet (Any): The packet to add.
+		"""
+		self.packet_buffer.append(packet)
+
+	def write_bytes(self, _bytes: bytes) -> None:
+		"""
+		Writes bytes to the output file, if it exists.
+
+		Args:
+			_bytes (bytes): The bytes to write.
+		"""
+		if self.output_file is not None:
+			self.output_file.write(_bytes)
+
+	def has_packets(self) -> bool:
+		"""
+		Checks if there are any packets in the packet buffer.
+
+		Returns:
+			bool: True if there are packets in the buffer, False otherwise.
+		"""
+		return bool(self.packet_buffer)
+
+	def get_next_packet(self) -> Optional[Any]:
+		"""
+		Gets the next packet from the packet buffer, if any.
+
+		Returns:
+			Optional[Any]: The next packet, or None if the buffer is empty.
+		"""
+		if self.has_packets():
+			return self.packet_buffer.popleft()
+		print("[REMParser] No packets in buffer")
+		return None
+
+	def parse_file(self, filepath: str, print_statistics: bool = True) -> None:
+		"""
+		Parses a file and optionally prints statistics about the parsed packets.
+
+		Args:
+			filepath (str): The path to the file to parse.
+			print_statistics (bool, optional): Whether to print statistics about the parsed packets. Defaults to True.
+		"""
+		print(f"[REMParser] Parsing file {filepath}")
+		with open(filepath, "rb") as file:
+			self.byte_buffer = file.read()
+			self.process()
+
+		if not print_statistics:
+			return
+
+		packet_counts: Dict[Type, int] = {}
+		packet_timestamps: Dict[Type, Dict[str, float]] = {}
+
+		print("   ", "PACKET TYPE".ljust(20), "COUNT", " ", "START DATE".ljust(23), " ", "STOP DATE".ljust(23), " ", "DURATION H:M:S:MS")
+		for packet in self.packet_buffer:
+			packet_type = type(packet)
+			packet_counts.setdefault(packet_type, 0)
+			packet_timestamps.setdefault(packet_type, {'start': packet.timestamp / 1000})
+			packet_counts[packet_type] += 1
+			packet_timestamps[packet_type]['stop'] = packet.timestamp / 1000
+			# print(packet.timestamp, packet_type.__name__)
+
+		for packet_type, count in packet_counts.items():
+			start_sec, stop_sec = packet_timestamps[packet_type].values()
+			start_msec, stop_msec = start_sec % 1, stop_sec % 1
+			datetime_str_start = datetime.fromtimestamp(np.floor(start_sec)).strftime("%Y-%m-%d %H:%M:%S")  + (f".{start_msec:.3f}"[2:])
+			datetime_str_stop  = datetime.fromtimestamp(np.floor(stop_sec )).strftime("%Y-%m-%d %H:%M:%S")  + (f".{stop_msec :.3f}"[2:])
+			duration_sec = stop_sec - start_sec
+			duration_str = str(timedelta(seconds=duration_sec))[:-3]
+			# if i'ts log, dont print data/duration stuff
+			if packet_type == BaseTypes.REM_Log:
+				print("   ", packet_type.__name__.ljust(20), str(count).rjust(5))
+			else:
+				print("   ", packet_type.__name__.ljust(20), str(count).rjust(5), " ", datetime_str_start, " ", datetime_str_stop, " ", duration_str)
+
+
+if __name__ == "__main__":
+	print("Running REMParser directly")
+
+	argparser = argparse.ArgumentParser()
+	argparser.add_argument('input_file', nargs='?', default='latest.rembin', help='File to parse')
+	args = argparser.parse_args()
+
+	print("Parsing file", args.input_file)
+
+	parser = REMParser(device=None)
+	parser.parse_file(args.input_file)
+
+	packet_dicts = []
+	for packet in parser.packet_buffer:
+		if type(packet) in [REM_RobotFeedback]:
+			packet_dict = utils.packet_to_dict(packet)
+			packet_dicts.append(packet_dict)
+
+	# Split up packets into types
+	packets_by_type = {}
+	for packet in parser.packet_buffer:
+		type_str = type(packet).__name__
+		if type_str not in packets_by_type:
+			packets_by_type[type_str] = []
+		packets_by_type[type_str].append(utils.packet_to_dict(packet))
+
+	output_file_no_ext = os.path.splitext(args.input_file)[0]
+
+	if args.input_file == "latest.rembin":
+		output_file_no_ext = os.path.join("logs", output_file_no_ext)
+
+	for type_str in packets_by_type:
+		packets = packets_by_type[type_str]
+		
+		# json
+		output_file_json = f"{output_file_no_ext}_{type_str}.json"
+		with open(output_file_json, 'w') as file:
+			file.write(json.dumps(packets))
+
+		# CSV
+		output_file_csv = f"{output_file_no_ext}_{type_str}.csv"
+		with open(output_file_csv, 'w') as file:
+			header = ",".join(list(packets[0].keys()))
+			file.write(header + "\n")
+			for packet in packets:
+				values = list(packet.values())
+				string = ",".join([str(v) for v in values])
+				file.write(string + "\n")
+
+	print("Done!")
---
+++ python_utils_new/newjoystick.py
@@ -0,0 +1,364 @@
+import os
+import math
+import sys
+import time
+import threading
+import argparse
+from datetime import datetime
+from glob import glob
+from typing import Callable, Dict, List, Optional
+
+import numpy as np
+# from xbox360controller import Xbox360Controller
+import pygame
+from pynput import keyboard
+
+# Add parent directory to path to allow importing from Core.Inc and REMParser
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Core.Inc.roboteam_embedded_messages.python import REM_BaseTypes as BaseTypes
+from Core.Inc.roboteam_embedded_messages.python.REM_Log import REM_Log
+from Core.Inc.roboteam_embedded_messages.python.REM_RobotFeedback import REM_RobotFeedback
+
+from REMParser import REMParser
+import utils
+
+MAX_SPEED = 1.0
+KICK_SPEED = 1.5
+ROTATION_SPEED = 3 # radians per second
+BASESTATION_FREQUENCY = 60 # ticks per second
+
+parser = argparse.ArgumentParser(description='Joystick to robot controller')
+parser.add_argument('-r', '--robot_ids', type=int, nargs='+', help='List of robot IDs to be controlled by the joysticks')
+parser.add_argument("--simulate", action="store_true", help="Simulate the basestation")
+args = parser.parse_args()
+
+
+class EventHandler:
+	"""Handles events and updates the status of the system."""
+
+	def __init__(self, shutdown: Callable[[], None]) -> None:
+		self.shutdown = shutdown
+		self.running = True
+		self.events: List[str] = []
+		self.batteryVoltages: dict = {}
+
+	def start(self, joystick_handler: 'JoystickHandler') -> None:
+		"""Starts the event handling loop."""
+		self.joystick_handler = joystick_handler
+		self.thread = threading.Thread(target=self.loop)
+		self.thread.start()
+
+	def record_event(self, id: int, event: str) -> None:
+		"""Records a new event."""
+		current_time = datetime.now().strftime("%H:%M:%S")
+		id = int(id) + 1
+		self.events.insert(0, f"[{id} | {current_time}] {event}")
+		self.events = self.events[:5]
+
+	def loop(self) -> None:
+		"""Main event handling loop."""
+		try:
+			while self.running:
+				string = "\r"
+				for id in self.joystick_handler.controllers:
+					controller = self.joystick_handler.controllers[id]
+					id = int(id) + 1
+					if controller.robot_ids:
+						string += f" Joystick {id} -> Robots {controller.robot_ids} | Voltages "
+						for r_id in controller.robot_ids:
+							if r_id in self.batteryVoltages:
+								string += f"{round(self.batteryVoltages[r_id], 2)}V "
+						string += f"| "
+					else:
+						string += f" Joystick {id} -> Robot {controller.robot_id} | "
+						if controller.robot_id in self.batteryVoltages:
+							string += f"Voltage {round(self.batteryVoltages[controller.robot_id],2)}V | "
+				print(string, end="")
+				for event in self.events:
+					print(event)
+				self.events = []
+				time.sleep(0.1)
+		except Exception as e:
+			self.record_event(-1, str(e))
+			print(e)
+			self.shutdown()
+
+
+class JoystickHandler:
+	"""Handles the detection and management of joysticks."""
+
+	def __init__(self, event_handler: EventHandler, shutdown: Callable[[], None], robot_ids: Optional[List[int]] = None) -> None:
+		self.shutdown = shutdown
+		self.running = True
+		self.controllers: Dict[int, 'Joystick'] = {}
+		self.lost_controllers: Dict[int, int] = {}
+		self.event_handler = event_handler
+		self.robot_ids = robot_ids
+		self.thread = threading.Thread(target=self.loop)
+		self.thread.start()
+
+	def loop(self) -> None:
+		"""Main loop for handling joysticks."""
+		try:
+			while self.running:
+				# Check if controllers in threads are still alive
+				for id in list(self.controllers.keys()):
+					if not self.controllers[id].controller._event_thread.is_alive():
+						self.event_handler.record_event(id, "Controller disconnected")
+						self.lost_controllers[id] = self.controllers[id].robot_id
+						del self.controllers[id]
+				# Discover new controllers that are not paired yet
+				for path in glob("/dev/input/js*"):
+					id = path.replace("/dev/input/js", "")
+					if id not in self.controllers:
+						try:
+							robot_id = self.lost_controllers.get(id, 0)
+							# controller = Xbox360Controller(id)
+							# wrapper = Joystick(controller, robot_id, self.robot_ids)
+							wrapper = Joystick(int(id), robot_id, self.robot_ids)
+
+							self.controllers[id] = wrapper
+							self.event_handler.record_event(id, "New controller discovered")
+						except Exception as e:
+							print(e)
+							time.sleep(1)
+							pass
+				time.sleep(0.1)
+		except Exception as e:
+			self.event_handler.record_event(-1, str(e))
+			print(f"\n{e}")
+			self.shutdown()
+
+class Joystick:
+    def __init__(self, joystick_index: int, robot_id: int, robot_ids: Optional[List[int]] = None) -> None:
+        self.id = joystick_index
+        self.joystick = pygame.joystick.Joystick(joystick_index)
+        self.joystick.init()
+
+        self.robot_id = robot_id
+        self.robot_ids = robot_ids
+        self.kick_speed = KICK_SPEED
+        self.dribblerOn = True
+        self.yaw = 0
+        self.ignore_joystick = 0
+
+        self.command = utils.generate_empty_robot_command()
+
+        self.assign_open_robot(1)
+
+    def assign_open_robot(self, addition: int = 0) -> None:
+        if self.robot_ids:
+            self.robot_id = 16
+        else:
+            claimed_ids = [joystick.robot_id for joystick in joystick_handler.controllers.values() if joystick.id != self.id]
+            while self.robot_id in claimed_ids:
+                self.robot_id = (self.robot_id + addition) % 16
+
+    def get_payload(self, keyboard_input: Dict[str, bool]) -> bytes:
+        pygame.event.pump()  # Refresh controller state
+
+        if self.ignore_joystick == 0:
+            if not self.robot_ids:
+                hat = self.joystick.get_hat(0)
+                if hat[0] != 0:
+                    self.robot_id = (self.robot_id + hat[0]) % 16
+                    self.assign_open_robot(addition=hat[0])
+
+            self.command.dribblerOn = self.dribblerOn
+
+            button_b = self.joystick.get_button(1)
+            trigger_r = self.joystick.get_button(7)
+
+            if button_b:
+                self.command.kickChipPower = self.kick_speed
+                self.command.doKick = True
+                self.command.doForce = True
+
+            if trigger_r:
+                self.command.kickChipPower = self.kick_speed
+                self.command.doKick = True
+                self.command.doForce = True
+
+            axis_rx = self.joystick.get_axis(2)  # Right stick X (corrected)
+            if abs(axis_rx) > 0.3:
+                self.yaw -= axis_rx * 0.1
+            
+            axis_lx = self.joystick.get_axis(0)
+            axis_ly = self.joystick.get_axis(1)
+
+
+
+            deadzone = 0.3
+            velocity_x = max(0, abs(axis_lx) - deadzone) / (1 - deadzone) * np.sign(axis_lx)
+            velocity_y = max(0, abs(axis_ly) - deadzone) / (1 - deadzone) * np.sign(axis_ly)
+
+            rho = math.sqrt(velocity_x ** 2 + velocity_y ** 2) * MAX_SPEED
+            theta = math.atan2(velocity_x, velocity_y)
+
+            self.command.toRobotId = self.robot_id
+            self.command.rho = rho
+            self.command.theta = theta + self.yaw + math.pi
+            self.command.yaw = self.yaw
+
+        else:
+            self.command.rho = 0
+            self.command.theta = 0
+            self.command.yaw = self.yaw
+
+        if any(keyboard_input.values()):
+            self.ignore_joystick = 120
+            if any(keyboard_input[str(i)] for i in range(10)):
+                self.robot_id = int([i for i in range(10) if keyboard_input[str(i)]][0])
+            self.yaw += (keyboard_input['q'] - keyboard_input['e']) * ROTATION_SPEED / BASESTATION_FREQUENCY
+            if keyboard_input['w'] or keyboard_input['s'] or keyboard_input['a'] or keyboard_input['d']:
+                velocity_x = (keyboard_input['w'] - keyboard_input['s']) * MAX_SPEED
+                velocity_y = (keyboard_input['a'] - keyboard_input['d']) * MAX_SPEED
+                rho = math.sqrt(velocity_x ** 2 + velocity_y ** 2)
+                theta = math.atan2(velocity_y, velocity_x)
+                self.command.rho = rho
+                self.command.theta = theta + self.yaw
+            else:
+                self.command.rho = 0
+                self.command.theta = 0
+            self.command.doKick = keyboard_input['k']
+            self.command.doForce = keyboard_input['k'] or keyboard_input['c']
+            self.command.kickChipPower = self.kick_speed
+            self.command.doChip = keyboard_input['c']
+            self.command.dribblerOn = keyboard_input['b']
+            self.command.yaw = self.yaw
+        else:
+            self.ignore_joystick = max(0, self.ignore_joystick - 1)
+
+        return self.command
+
+class BasestationHandler:
+	"""Handles communication with the basestation."""
+
+	def __init__(self, event_handler: EventHandler, joystick_handler: JoystickHandler, shutdown: Callable[[], None], simulate: bool) -> None:
+		self.shutdown = shutdown
+		self.packet_Hz = BASESTATION_FREQUENCY
+		self.running = True
+		if args.simulate:
+			self.basestation = utils.open_simulated_basestation()
+		else:
+			self.basestation = utils.open_continuous(timeout=0.01)
+		self.event_handler = event_handler
+		self.joystick_handler = joystick_handler
+		self.thread = threading.Thread(target=self.loop)
+		self.thread.start()
+
+	def loop(self) -> None:
+		print("starting base loop")
+		"""Main loop for handling basestation communication."""
+		try:
+			current_dir = os.path.dirname(os.path.abspath(__file__))
+			log_dir = os.path.join(current_dir, "logs/joystick")
+			os.makedirs(log_dir, exist_ok=True)
+			filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".rembin"
+			logger = REMParser(self.basestation, f"{log_dir}/{filename}")
+
+			last_written = time.time()
+			while self.running:
+				time_till_next_tick = last_written + 1. / self.packet_Hz - time.time()
+				time.sleep(max(0, time_till_next_tick))
+				last_written += 1. / self.packet_Hz
+
+				for joystick in joystick_handler.controllers.values():
+					payload = joystick.get_payload(keyboard_handler.get_keyboard_input())
+					for i in joystick.robot_ids if joystick.robot_ids else [payload.toRobotId]:
+						payload.toRobotId = i
+						self.basestation.write(payload)
+						logger.write_bytes(payload.encode())
+
+				logger.read()
+				logger.process()
+
+				def handle_rem_log(rem_log: REM_Log) -> None:
+					log_from = "[?]  "
+					if rem_log.fromBS:
+						log_from = "[BS] "
+					if not rem_log.fromPC and not rem_log.fromBS:
+						log_from = f"[{str(rem_log.fromRobotId).rjust(2)}] "
+
+					message = rem_log.message.strip()
+					message = log_from + message
+
+					nwhitespace = os.get_terminal_size().columns - len(message) - 2
+					print(f"\r{message}{' ' * nwhitespace}")
+
+				def handle_rem_robotfeedback(rem_robotfeedback: REM_RobotFeedback) -> None:
+					self.event_handler.batteryVoltages[rem_robotfeedback.fromRobotId] = rem_robotfeedback.batteryLevel
+
+				while logger.has_packets():
+					packet = logger.get_next_packet()
+					if isinstance(packet, REM_Log):
+						handle_rem_log(packet)
+					elif isinstance(packet, REM_RobotFeedback):
+						handle_rem_robotfeedback(packet)
+		except Exception as e:
+			self.event_handler.record_event(-1, str(e))
+			print(e)
+			self.shutdown()
+
+
+class KeyboardHandler:
+	"""Handles keyboard input and controls the robots."""
+
+	def __init__(self, joystick_handler: JoystickHandler) -> None:
+		self.joystick_handler = joystick_handler
+		self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+		self.listener.start()
+		self.keyboard_input: Dict[str, bool] = {'w': False, 's': False, 'a': False, 'd': False, 'k': False, 'c': False, 'b': False, 'q': False, 'e': False, '1' : False, '2' : False, '3' : False, '4' : False, '5' : False, '6' : False, '7' : False, '8' : False, '9' : False, '0' : False}
+
+	def on_press(self, key: keyboard.Key) -> None:
+		"""Handles key press events."""
+		try:
+			char = key.char
+			if char in self.keyboard_input:
+				self.keyboard_input[char] = True
+		except AttributeError:
+			pass
+
+	def on_release(self, key: keyboard.Key) -> None:
+		"""Handles key release events."""
+		try:
+			char = key.char
+			if char in self.keyboard_input:
+				self.keyboard_input[char] = False
+		except AttributeError:
+			pass
+
+	def get_keyboard_input(self) -> Dict[str, bool]:
+		"""Returns the current keyboard input."""
+		return self.keyboard_input
+
+
+def shutdown() -> None:
+	"""Shuts down the system."""
+	print("Exiting")
+	event_handler.running = False
+	basestation_handler.running = False
+	joystick_handler.running = False
+	for joystick in joystick_handler.controllers.values():
+		joystick.controller.close()
+
+
+event_handler = EventHandler(shutdown)
+
+
+def thread_exception_handler(args: threading.ExceptHookArgs) -> None:
+	"""Handles exceptions raised in threads."""
+	event_handler.record_event(-1, f"Caught: {args}")
+
+threading.excepthook = thread_exception_handler
+
+joystick_handler = JoystickHandler(event_handler, shutdown, args.robot_ids)
+basestation_handler = BasestationHandler(event_handler, joystick_handler, shutdown, args.simulate)
+keyboard_handler = KeyboardHandler(joystick_handler)
+event_handler.start(joystick_handler)
+
+try:
+	event_handler.thread.join()
+except:
+	shutdown()
